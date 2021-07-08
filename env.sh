06cb78efa81fcae8bd9f727fbd1ebe04090dc938
Juan Manuel Parrilla <juanma@auchentoshan>
2021-07-08 13:26:44 +0000

    Updating installation instructions

--- env.sh
+++ env.sh
@@ -1,11 +1,11 @@
-#CONDA_ENV_NAME=electrondensity
+CONDA_ENV_NAME=electrondensity
 export PROJECT_ROOT=`pwd`
 export DATA_DIR='./data'
 export LOGS_DIR='./logs'
 export MODELS_DIR='./models'
 export CPU_COUNT=-1
 
-#conda activate $CONDA_ENV_NAME
+conda activate $CONDA_ENV_NAME
 
 export ORBKITPATH=$PROJECT_ROOT/orbkit
 
