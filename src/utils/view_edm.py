da916730e4d889928db426b586f34e22da8f7b53
Juanma <juanma@home>
2021-07-15 10:06:26 +0100

    Nothing important

--- src/utils/view_edm.py
+++ src/utils/view_edm.py
@@ -47,20 +47,33 @@ if __name__ == "__main__":
     with open(args.file, 'rb') as pfile:
         cubes = pickle.load(pfile)
 
-    if len(cubes) == 2:
-        orig = cubes[0]  # original EDMs from the validation set
-        gene = cubes[1]  # generated EDMs from the validation set. Related 1 to 1 to orig
+        ed = cubes['electron_density']
+        esp = cubes['electrostatic_potential']
 
-        for i in range(args.render):
-            output.view_with_mayavi(grid.x, grid.y, grid.z, orig[i, :, :, :, 0])
-            output.view_with_mayavi(grid.x, grid.y, grid.z, gene[i, :, :, :, 0])
+        from scipy.ndimage import gaussian_filter, grey_dilation, uniform_filter
+        espn = grey_dilation(esp*-1, size=3)
+        espp = grey_dilation(esp, size=3)
+        esp = espn*-1 + espp
+        esp = gaussian_filter(esp, sigma=1)
+        # esp = uniform_filter(esp, size=3)
+        output.view_with_mayavi(grid.x, grid.y, grid.z, esp[:,:,:])
+        #output.view_with_mayavi(grid.x, grid.y, grid.z, orig[i, :, :, :, 0])
 
-    elif len(cubes) == 4:
-        for i in range(args.render):
-            output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[0][i, :, :, :, 0])
-            output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[1][i, :, :, :, 0])
-            output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[2][i, :, :, :, 0])
-            output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[3][i, :, :, :, 0])
-    else:
-        for i in range(args.render):
-            output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[i][:, :, :, 0])
+
+    # if len(cubes) == 2:
+    #     orig = cubes[0]  # original EDMs from the validation set
+    #     gene = cubes[1]  # generated EDMs from the validation set. Related 1 to 1 to orig
+
+    #     for i in range(args.render):
+    #         output.view_with_mayavi(grid.x, grid.y, grid.z, orig[i, :, :, :, 0])
+    #         output.view_with_mayavi(grid.x, grid.y, grid.z, gene[i, :, :, :, 0])
+
+    # elif len(cubes) == 4:
+    #     for i in range(args.render):
+    #         output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[0][i, :, :, :, 0])
+    #         output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[1][i, :, :, :, 0])
+    #         output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[2][i, :, :, :, 0])
+    #         output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[3][i, :, :, :, 0])
+    # else:
+    #     for i in range(args.render):
+    #         output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[i][:, :, :, 0])
