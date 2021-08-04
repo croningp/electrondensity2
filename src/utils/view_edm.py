4dcd683dd60ccdca679f849a21bb9789474d29bd
Juanma <juanma@home>
2021-08-04 11:42:55 +0100

    Pd cage ESP seems to be OK. Next step will be to optimise

--- src/utils/view_edm.py
+++ src/utils/view_edm.py
@@ -8,6 +8,8 @@
 
 import pickle
 import argparse
+from tensorflow.nn import max_pool3d
+import numpy as np
 
 from orbkit import grid, output
 
@@ -22,39 +24,55 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print('Opening {}'.format(args.file))
 
-    set_grid(64, 0.5)
+    set_grid(80, 0.5)
     grid.init_grid()
 
     with open(args.file, 'rb') as pfile:
         cubes = pickle.load(pfile)
 
-        ed = cubes['electron_density']
-        esp = cubes['electrostatic_potential']
+    cubes = np.float32(cubes)
+    cubes = np.expand_dims(cubes, axis=[0,-1])
+    output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[0,:,:,:,0])
+    datap = max_pool3d(cubes, 5, 1, 'SAME')
+    datan = max_pool3d(cubes*-1, 5, 1, 'SAME')
+    cubes = datap + datan*-1
+    output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[0,:,:,:,0])
 
-        from scipy.ndimage import gaussian_filter, grey_dilation, uniform_filter
-        espn = grey_dilation(esp*-1, size=3)
-        espp = grey_dilation(esp, size=3)
-        esp = espn*-1 + espp
-        esp = gaussian_filter(esp, sigma=1)
-        # esp = uniform_filter(esp, size=3)
-        output.view_with_mayavi(grid.x, grid.y, grid.z, esp[:,:,:])
-        #output.view_with_mayavi(grid.x, grid.y, grid.z, orig[i, :, :, :, 0])
+    # ed = cubes['electron_density']
+    # esp = cubes['electrostatic_potential']
+
+    # from scipy.ndimage import gaussian_filter, grey_dilation, uniform_filter
+    # espn = grey_dilation(esp*-1, size=3)
+    # espp = grey_dilation(esp, size=3)
+    # esp = espn*-1 + espp
+    # esp = gaussian_filter(esp, sigma=1)
+    # # esp = uniform_filter(esp, size=3)
+    # output.view_with_mayavi(grid.x, grid.y, grid.z, esp[:,:,:])
+    # #output.view_with_mayavi(grid.x, grid.y, grid.z, orig[i, :, :, :, 0])
 
 
-    # if len(cubes) == 2:
-    #     orig = cubes[0]  # original EDMs from the validation set
-    #     gene = cubes[1]  # generated EDMs from the validation set. Related 1 to 1 to orig
+    if len(cubes) == 2:
+        orig = cubes[0]  # original EDMs from the validation set
+        gene = cubes[1]  # generated EDMs from the validation set. Related 1 to 1 to orig
 
-    #     for i in range(args.render):
-    #         output.view_with_mayavi(grid.x, grid.y, grid.z, orig[i, :, :, :, 0])
-    #         output.view_with_mayavi(grid.x, grid.y, grid.z, gene[i, :, :, :, 0])
+        for i in range(args.render):
+            output.view_with_mayavi(grid.x, grid.y, grid.z, orig[i, :, :, :, 0])
+            output.view_with_mayavi(grid.x, grid.y, grid.z, gene[i, :, :, :, 0])
 
-    # elif len(cubes) == 4:
-    #     for i in range(args.render):
-    #         output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[0][i, :, :, :, 0])
-    #         output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[1][i, :, :, :, 0])
-    #         output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[2][i, :, :, :, 0])
-    #         output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[3][i, :, :, :, 0])
-    # else:
-    #     for i in range(args.render):
-    #         output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[i][:, :, :, 0])
+    elif len(cubes) == 3:
+        # prepare original esp for visualization
+        datap = max_pool3d(cubes[1], 5, 1, 'SAME')
+        datan = max_pool3d(cubes[1]*-1, 5, 1, 'SAME')
+        origesps = datap + datan*-1
+
+        for i in range(args.render):
+            ed = cubes[0][i, :, :, :, 0]
+            geneesp = cubes[2][i, :, :, :, 0]
+            origesp = origesps[i, :, :, :, 0]
+
+            output.view_with_mayavi(grid.x, grid.y, grid.z, ed)
+            output.view_with_mayavi(grid.x, grid.y, grid.z, origesp)
+            output.view_with_mayavi(grid.x, grid.y, grid.z, geneesp)
+    else:
+        for i in range(args.render):
+            output.view_with_mayavi(grid.x, grid.y, grid.z, cubes[i][:, :, :, 0])
