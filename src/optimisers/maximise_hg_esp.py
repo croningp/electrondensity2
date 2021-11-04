eb4aee4371ec143bb4e3ce1974f03b3f0437fb56
Juan Manuel Parrilla <juanma@auchentoshan>
2021-11-04 17:23:32 +0000

    trying to do more cb6 optimisations

--- src/optimisers/maximise_hg_esp.py
+++ src/optimisers/maximise_hg_esp.py
@@ -14,6 +14,7 @@ import os
 import tqdm
 import pickle
 import numpy as np
+from datetime import datetime
 import tensorflow as tf
 from tensorflow.keras import backend as K
 
@@ -189,6 +190,20 @@ def grad_esp_overlapping(noise, vae, ed2esp, hostesp):
 
 if __name__ == "__main__":
 
+    # factor that we will use to multiply the ED part of gradient descent.
+    # The ESP part will by multiplied by 1-ed_factor
+    ed_factor = 0.9
+
+    # folder where to save the logs of this run
+    startdate = datetime.now().strftime('%Y-%m-%d')
+    RUN_FOLDER = startdate +'_'+ str(ed_factor)
+
+    # just do a while loop to make sure the folder doesnt exist
+    n = 0
+    while os.path.exists(RUN_FOLDER+'_'+str(n)+'/'):
+        n += 1
+    os.mkdir(RUN_FOLDER+'_'+str(n)+'/')
+
     BATCH_SIZE = 32
     DATA_FOLDER = '/home/nvme/juanma/Data/Jarek/'
 
@@ -199,38 +214,45 @@ if __name__ == "__main__":
 
     noise_t = K.random_uniform(shape = (BATCH_SIZE, z_dim),
                              minval = -2.0, maxval = 2.0)
-    _, _, initial_output = grad_ed_overlapping(noise_t, vae, host_ed)
+    _, _, init_eds, init_esps = grad_esp_overlapping(noise_t, vae, ed_to_esp, host_esp)
 
-    with open('cage_esp_opt_initial.p', 'wb') as file:
-        pickle.dump(initial_output, file)
+    with open('cb6_esp_opt_initial_g.p', 'wb') as file:
+        pickle.dump(RUN_FOLDER+init_eds, file)
+
+    with open('cb6_esp_opt_initial_g_esp.p', 'wb') as file:
+        pickle.dump(RUN_FOLDER+init_esps, file)
+
+    with open('cb6_esp_opt_initial_hg.p', 'wb') as file:
+        pickle.dump(RUN_FOLDER+init_eds+host_ed, file)
 
     # we will do five cycles of optimising
     for factor in [1, 5, 10, 20, 50]:
         lr = 0.05 / factor
+        slr = str(lr)
 
         for j in tqdm.tqdm(range(int(10000/factor))):
             # try to minimise overlapping ESP
             f, grads, output, esps = grad_esp_overlapping(noise_t, vae, ed_to_esp, host_esp)
             print(np.mean(f.numpy()))
-            noise_t -= lr * grads[0].numpy() * 0.5
+            noise_t -= lr * grads[0].numpy() * ed_factor
             noise_t = np.clip(noise_t, a_min=-5.0, a_max=5.0)
 
             if j % 1000 == 0:
-                with open('cage_esp_optimizedESPED.p', 'wb') as file:
-                    pickle.dump(output, file)
-                with open('cage_esp_optimizedESP.p', 'wb') as file:
-                    pickle.dump(esps, file)
+                with open('cage_esp_optimizedESPED'+slr+'.p', 'wb') as file:
+                    pickle.dump(RUN_FOLDER+output, file)
+                with open('cage_esp_optimizedESP'+slr+'.p', 'wb') as file:
+                    pickle.dump(RUN_FOLDER+esps, file)
 
             # try to minimise overlapping ED
             f, grads, output = grad_ed_overlapping(noise_t, vae, host_ed)
             print(np.mean(f.numpy()))
-            noise_t -= lr * grads[0].numpy()
+            noise_t -= lr * grads[0].numpy() * (1-ed_factor)
             noise_t = np.clip(noise_t, a_min=-5.0, a_max=5.0)
 
             if j % 1000 == 0:
-                with open('cage_esp_optimizedEDED.p', 'wb') as file:
-                    pickle.dump(output, file)
+                with open('cage_esp_optimizedEDED'+slr+'.p', 'wb') as file:
+                    pickle.dump(RUN_FOLDER+output, file)
 
-    with open('cage_esp_optimized.p', 'wb') as file:
-        pickle.dump(output, file)
+    with open('cage_esp_optimized_final.p', 'wb') as file:
+        pickle.dump(RUN_FOLDER+output, file)
 
