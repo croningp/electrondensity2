87ae23fff22701d50ed0403cfb49f44fae1d8a81
Andrew Abi-Mansour <andrew.gaam@gmail.com>
2024-07-10 15:00:58 -0500

    remove orbkit installation

--- env.sh
+++ env.sh
@@ -7,18 +7,4 @@ export CPU_COUNT=-1
 
 conda activate $CONDA_ENV_NAME
 
-export ORBKITPATH=$PROJECT_ROOT/orbkit
-
-if [ ! -d "$ORBKITPATH" ]
-then
-	echo "Couldn't find orbkit. Downloading and installing..."
-	git clone https://github.com/orbkit/orbkit.git
-	cd $ORBKITPATH
-	git checkout cc17072
-	python setup.py build_ext --inplace clean
-	cd $PROJECT_ROOT
-fi
-
-export PYTHONPATH=$PYHONPATH:$ORBKITPATH
-
 
